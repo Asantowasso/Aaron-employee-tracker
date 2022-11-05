247173676f3b003f692be26e9378792c2754629e
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-11-05 15:14:10 -0400

    added question and sql files

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 //First, give the following options view all departments, all roles, all employees, add a department, add a role and update an employee role
 
+const inquirer = require("inquirer");
+
+const userOptions = () => {
+  inquirer.prompt([
+    {
+      name: "A list of options",
+      type: "list",
+      message: "select an option",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "Add a department",
+        "add a role",
+        "Update an employee role",
+      ],
+    },
+  ]);
+};
+userOptions();
+
+// function init() {
+//     inquirer.prompt(userOptions)
+//     .then ((inquirerResponse) => {
+//     console.log(inquirerResponse)
+
+// })
+// }
+// // Function call to initialize app
+// init()
